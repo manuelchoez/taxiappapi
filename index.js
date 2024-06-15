7022c43b686d51c2feb178992ff375debf1cc38c
Manuel Choez <40345380+manuelchoez@users.noreply.github.com>
2024-06-15 11:39:53 -0500

    primer metodo

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 
 const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send(true);
+});
+
 app.listen(4000, () => { console.log('Server is running on port 4000'); });
